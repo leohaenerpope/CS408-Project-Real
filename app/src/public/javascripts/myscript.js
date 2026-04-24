07c9cab49ab44518ecdbb38e0ac86fb5fc29be0b
Leo Haener-Pope <leohaenerpope@u.boisestate.edu>
2026-04-24 15:47:28 -0600

    Delete button better styling and functionality for matchup notes and players

--- app/src/public/javascripts/myscript.js
+++ app/src/public/javascripts/myscript.js
@@ -1,3 +1,10 @@
-function showHelloWorld() {
-  alert('helloworld');
+function startDeleteAsk(url, message) {
+  const form = document.getElementById('deleteForm');
+  const msg = document.getElementById('deleteMessage');
+
+  form.action = url;
+  msg.textContent = message;
+
+  const modal = new bootstrap.Modal(document.getElementById('confirmDeleteModal'));
+  modal.show();
 }
